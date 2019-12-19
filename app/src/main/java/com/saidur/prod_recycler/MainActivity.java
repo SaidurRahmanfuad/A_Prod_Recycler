a099b96c49a772b9ee246593207974877ce24a3c
Saidur Rahman <47972954+SaidurRahmanfuad@users.noreply.github.com>
2019-12-20 03:31:57 +0600

    Last upload 20/12/2019@saidur

--- app/src/main/java/com/saidur/prod_recycler/MainActivity.java
+++ app/src/main/java/com/saidur/prod_recycler/MainActivity.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,11 +35,15 @@ import static com.saidur.prod_recycler.R.layout.*;
 public class MainActivity extends AppCompatActivity {
     TabLayout tabMenu;
     ViewPager viewPager;
+    DrawerLayout drawer;
+    FloatingActionButton fabmenu,fabadd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_main);
-
+        fabmenu=findViewById(id.fab_menu);
+        fabadd=findViewById(id.fab_add);
+        drawer = findViewById(R.id.drawer);
         tabMenu     = findViewById(R.id.tabMenu);
         viewPager   = findViewById(R.id.viewPager);
 
@@ -45,8 +51,33 @@ public class MainActivity extends AppCompatActivity {
 
         tabMenu.setupWithViewPager(viewPager);
         viewPager.setCurrentItem(0);  // 0 = sell , 1=Buy
-
+        fabclick();
     }
+
+    private void fabclick() {
+        fabmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(drawer.isDrawerOpen(Gravity.RIGHT))
+                {
+                    drawer.closeDrawer(Gravity.RIGHT);
+                }
+                else
+                {
+                    drawer.openDrawer(Gravity.RIGHT);
+                }
+            }
+        });
+
+        fabadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new Buy_Fragment(),"Buy Items");
@@ -54,5 +85,6 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
     }
+
 
 }
